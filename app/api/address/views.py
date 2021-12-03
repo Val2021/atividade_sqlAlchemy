a290f9eb309ae57acec60fd12e196c3c146e6bd1
Val Araujo <val.araujo@businessplanner.com.br>
2021-12-03 19:50:34 -0300

    Implementado delete address

--- app/api/address/views.py
+++ app/api/address/views.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends,status
 from app.models.models import Address
 from .schemas import AddressSchema, ShowAddressSchema
 from app.repositories.address_repository import AddressRepository
+from fastapi.exceptions import HTTPException
 from  typing import List
 
 
@@ -24,3 +25,10 @@ def update(id:int,address:AddressSchema,repository: AddressRepository = Depends(
 @router.get('/{id}')
 def show(id:int, repository: AddressRepository = Depends()):
     return repository.get_by_id(id)
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_coupon(id: int,repository: AddressRepository = Depends()):
+    result=repository.delete(id)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = f'Endereço com o id:{id} não encontrado')
